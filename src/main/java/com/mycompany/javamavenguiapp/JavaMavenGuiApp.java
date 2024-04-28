04b2a0df41f014c4e75841be20c4bca7862e7ae5
Johna <johnpeterkrause@gmail.com>
2024-04-28 15:20:17 +0200

    Setup initial Java Maven project using Swing GUI framework

---
+++ src/main/java/com/mycompany/javamavenguiapp/JavaMavenGuiApp.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javamavenguiapp;
+
+
+/**
+ *
+ * @author johna
+ */
+public class JavaMavenGuiApp {
+
+
+    public static void main(String[] args) {
+
+    }
+}
